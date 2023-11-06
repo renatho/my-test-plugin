796eb0a44a804f59ec3b89d2d64bf4d52cd9246e
Renatho De Carli Rosa <renatho@gmail.com>
2023-11-06 11:11:14 -0300

    Initial commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'bb97562038a152c2ed8b');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '49d50f994115e74c70d3');
